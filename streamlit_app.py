da1110d285d68d5ce1a98490c16c866921d69967
sleuthingsquad <139034655+sleuthingsquad@users.noreply.github.com>
2024-10-01 15:43:16 +0000

    output working

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pandas as pd
-import os
 
 
 #getting data
 data_path  = "Design_Unit1_database_CSV.csv"
 excel_data = pd.read_csv(data_path)
+    # a CSV file was used to avoid bugs
+    #an excel file needs pip to install openpyxl, which resulted in an error
 
 #testing functions
 def testing():
@@ -29,24 +30,27 @@ st.subheader("Your Topic")
 # getting input and using it to generate output
 
 def clickedFunc():
-    #clearing screen to print output at bottom of the page
-    st.empty()
+    
+    st.write("Here are some suggestions for you:")
 
     #generating output
-    #st.write(excel_data.loc[excel_data.Topic==option])
-    # name = excel_data.loc[excel_data.Topic == option]["Name"] 
-    # names_list = name.tolist()
+       
+    name = excel_data.loc[excel_data.Topic == option]["Name"] 
+    names_list = name.tolist()
 
-    # year = excel_data.loc[excel_data.Topic == option]["Year"]
-    # dates_list = year.tolist()
+    year = excel_data.loc[excel_data.Topic == option]["Year"]
+    dates_list = year.tolist()
 
-    # description = excel_data.loc[excel_data.Topic == option]["Description"]
-    # desc_list = description.tolist()
+    description = excel_data.loc[excel_data.Topic == option]["Description"]
+    desc_list = description.tolist()
 
-    # i = 0
-    # while i < len(names_list):
-    #     st.write(f"- {names_list[i]} ({dates_list[i]}): {desc_list[i]}")
-    #     i+=1
+    #printing output
+
+    i = 0
+    while i < len(names_list):
+        st.write(f"- {names_list[i]} ({dates_list[i]}): {desc_list[i]}")
+        i+=1
+
 
 #getting input
 option = st.selectbox(
@@ -55,7 +59,12 @@ option = st.selectbox(
     placeholder="Your movie topic"
 )
 
-st.button("Generate Suggestions", on_click = clickedFunc)
+#st.button("Gen", on_click = clickedFunc)
+    #using the above code results in the output being printed at the top of the page 
+    #thus, the below piece of code is used
+if st.button("Generate Suggestions"):
+    clickedFunc()
+
 
 # """
 # # My first app
